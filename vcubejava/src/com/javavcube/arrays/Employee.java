f6cec918955e0f87e0ad3735bf516b9929441471
Malavath Nithin <nithinmalavath3233@gmail.com>
2026-08-26 10:51:09 +0530

    encapsulation

---
+++ vcubejava/src/com/javavcube/arrays/Employee.java
@@ -0,0 +1,31 @@
+package com.javavcube.arrays;
+
+public class Employee {
+
+	private int eid;
+	private String ename;
+	private Double salary;
+	public void setEid(int eid) {
+		this.eid=eid;
+	}
+	public int getEid() {
+		return eid;
+	}
+	public void setEname(String ename) {
+		this.ename=ename;
+	}
+	public String getEname() {
+		return ename;
+	}
+	
+	public void setSalary(double salary) {
+		this.salary=salary;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	
+	
+	
+
+}
---
+++ vcubejava/src/com/javavcube/arrays/TestDriver.java
@@ -0,0 +1,18 @@
+package com.javavcube.arrays;
+
+public class TestDriver {
+
+	public static void main(String[] args) {
+		Employee emp1=new Employee();
+		emp1.setEid(18);
+		emp1.setEname("rajesh");
+		emp1.setSalary(193953);
+		
+		System.out.println(emp1.getEid());
+		System.out.println(emp1.getEname());
+		System.out.println(emp1.getSalary());
+
+
+	}
+
+}
