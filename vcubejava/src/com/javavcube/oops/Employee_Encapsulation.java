fb7caf8ae3640c4abfe0c2489083df182d9352d6
Malavath Nithin <nithinmalavath3233@gmail.com>
2026-08-29 21:26:45 +0530

    oops practice01

---
+++ vcubejava/src/com/javavcube/oops/Employee_Encapsulation.java
@@ -0,0 +1,38 @@
+package com.oops;
+
+public class Employee_Encapsulation {
+
+	private int id;
+	private String name;
+	private double salary;
+	private String password;
+	public int getId() {
+		return id;
+		
+	}
+	public void setId(int id ) {
+		this.id=id;
+	}
+	public String getName() {
+		return name;
+		
+	}
+	public void setName(String name ) {
+		this.name=name;
+	}
+	public double getSalary() {
+		return salary;
+		
+	}
+	public void setSalary(double salary ) {
+		this.salary=salary;
+	}
+	public String getPassword() {
+		return password;
+		
+	}
+	public void setPassword(String password ) {
+		this.password=password;
+	}
+
+}
