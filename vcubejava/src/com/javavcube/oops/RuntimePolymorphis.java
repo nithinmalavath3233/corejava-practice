6c99c47bdb33ab4c7d5daf0b98e36af0ebc6cccb
Malavath Nithin <nithinmalavath3233@gmail.com>
2026-09-06 23:36:58 +0530

    runtime polymorphism

---
+++ vcubejava/src/com/javavcube/oops/RuntimePolymorphis.java
@@ -0,0 +1,28 @@
+package com.javavcube.oops;
+
+
+
+class Animal {
+
+    void sound() {
+        System.out.println("Animal makes a sound");
+    }
+}
+
+class Dog extends Animal {
+
+    @Override
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
+
+public class RuntimePolymorphism {
+
+    public static void main(String[] args) {
+
+        Animal obj = new Dog();
+
+        obj.sound();
+    }
+}
