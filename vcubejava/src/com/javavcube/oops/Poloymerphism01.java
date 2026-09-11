8b5c221e1f9b113744e23d77058ee8bfd65c6dbd
Malavath Nithin <nithinmalavath3233@gmail.com>
2026-09-11 23:43:34 +0530

    polymerphism practice 01

---
+++ vcubejava/src/com/javavcube/oops/Poloymerphism01.java
@@ -0,0 +1,19 @@
+package com.javavcube.oops;
+
+public class Poloymerphism01 {
+
+	public static void main(String[] args) {
+		Poloymerphism01 p1= new Poloymerphism01();
+		System.out.println(100);
+		System.out.println(84032843092L);
+		System.out.println(5.9F);
+		System.out.println(8.5D);
+		char ch[]= {'a','b','c'};
+		System.out.println('m');
+		System.out.println(ch);
+		System.out.println(false);
+		
+
+	}
+
+}
